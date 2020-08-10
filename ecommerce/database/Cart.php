9105cf44b013d998fccac0417172702be8ffd449
weihaoke13 <weihaoke13@gmail.com>
2020-08-09 17:34:05 -0700

    add to cart

--- ecommerce/database/Cart.php
+++ ecommerce/database/Cart.php
@@ -3,5 +3,21 @@
 
 class Cart
 {
+    public $db = null;
+
+    //access database
+    public function __construct(DBController $db){
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // insert into cart table
+    public function insertIntoCart($params = null, $table = "cart"){
+
+    }
+
+
+
+
 
 }
